c2919b8b5281752d318f8ccf9d2603c2e31f3efb
Naren Shridhar <naren.shridhar@infor.com>
2019-01-04 11:14:03 -0500

    Chapter21Problem5.java

---
+++ Chapter21Problem5.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Chapter21Problem5
+{
+	public static void main (String[] args) throws FileNotFoundException
+	{
+		//String filename = "/Users/NarenShridhar/Documents/JavaPrograms/ChapterTwentyOneProgrammingChallenges/TracingGenealogies.txt";
+
+		TracingGenealogies.genealogyTracer();
+
+		
+	}
+}
